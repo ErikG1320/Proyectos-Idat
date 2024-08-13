a38d722552f89c83fa8efbad159a71874ea2b434
ErikG1320 <158307983+ErikG1320@users.noreply.github.com>
2024-08-13 13:47:51 -0500

    Actualizar TiendaRepository.java

--- idat/src/main/java/com/example/demo/Repository/TiendaRepository.java
+++ idat/src/main/java/com/example/demo/Repository/TiendaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.Entity.TiendaMicas;
 
-public interface TiendaRepository extends JpaRepository<TiendaMicas, Long> {
+public interface TiendaRepository extends JpaRepository<TiendaMicas, Integer> {
     TiendaMicas findByNombre(String nombre);
     TiendaMicas findByNombreModelo(String nombreModelo);
     Optional<TiendaMicas> findById(int id);
